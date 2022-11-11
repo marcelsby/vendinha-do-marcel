9ee935fa613ea6fa85b910ba8f44c58ffe4f8c8b
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-11-11 16:34:07 -0300

    refactor: toggle nav links logic and animations in navbar show event

--- src/menu.js
+++ src/menu.js
@@ -3,10 +3,10 @@ const dropdownToggleButton = document.querySelector("#dropdown-toggle-button");
 dropdownToggleButton.addEventListener("click", () => {
   const dropdownLinks = document.querySelector("#dropdown-links");
 
-  dropdownLinks.style.display =
-    dropdownLinks.style.display != "flex" ? "flex" : "none";
+  dropdownLinks.classList.toggle("hide");
+  dropdownLinks.classList.toggle("show-flex");
 
-  const isMenuOpen = dropdownLinks.style.display == "flex";
+  const isMenuOpen = dropdownLinks.classList.contains("show-flex");
 
   if (isMenuOpen) {
     dropdownToggleButton.classList.replace("ph-list", "ph-x");
